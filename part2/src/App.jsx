f1d0b9321e4cdbb3a169d498bec2eca04d1c00f8
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2024-09-24 09:43:53 +0000

    list

---
+++ part2/src/App.jsx
@@ -0,0 +1,21 @@
+import TitleComponent from "./TitleComponent"
+
+function App() {
+  
+
+  return (
+    <>      <div>
+        <TitleComponent> </ TitleComponent>
+      </div >
+      <div>
+        <li key ={0}>HTML</li>
+        <li key ={1}>CSS</li>
+        <li key ={2}>JS</li>
+        <li key ={3}>React</li>
+      </div>
+      </>
+
+  )
+}
+
+export default App;
---
+++ part2/src/TitleComponent.jsx
@@ -0,0 +1,5 @@
+function TitleComponent(){
+    return (<h1>Hello, JSX</h1>)
+}
+export default TitleComponent;
+
