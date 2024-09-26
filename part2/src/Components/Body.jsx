deac1e711d55021ec901582f979c71954a153d40
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2024-09-26 06:58:31 +0000

    addedToggle

--- part2/src/Components/Body.jsx
+++ part2/src/Components/Body.jsx
@@ -4,11 +4,13 @@ import { useState } from 'react'
 
 export const Body = () => {
     const [listOfRest, setListOfRest] = useState(restaurantList);
+    //below have passed the default value for searchInput
+    const [searchInput, setSearchInput] = useState("KFC");
 
     return (
-    
+
         <div className="body">
-              
+
             <button className="res-filter" onClick={() => {
                 const filteredList = restaurantList.filter((rest) => rest.data.avgRating > 4);
                 setListOfRest(filteredList);
@@ -23,10 +25,30 @@ export const Body = () => {
             }>Low rated rest
             </button>
 
+            <div className="search-container">
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="search"
+                    value={searchInput}
+                    onChange={(e) => { setSearchInput(e.target.value) }}
+                />
+                <button
+                    className="search-btn"
+                    onClick={() => {
+                        if (searchInput === "false") {
+                            setSearchInput("true")
+                        } else {
+                            setSearchInput("false")
+                        }
+                    }}
+                >Search </button>
+            </div>
+            <h1>
+                {searchInput}
+            </h1>
 
-            <div className = "search-container">
-                <input type="text" className="search-input" placeholder="search" />
-              </div>
+
             <div className="res-container">
                 {
                     listOfRest.map((rest) => (
